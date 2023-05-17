4f5b85d5bfe51c7c1acb6f2e2791fcf4334e0197
Admin <Admin@TEB127006>
2023-05-17 10:43:40 +0200

    Add project files.

---
+++ Metoder Uppgift 6.11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur stora sidor ska kvadraten ha? : ");
+            int Sida=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Vilket tecken ska kvadraten ha? : ");
+            string tecken = Console.ReadLine();
+            Console.Write("Hur stort mellanrum ska det vara? : ");
+            int mellanrum = Convert.ToInt32(Console.ReadLine());
+            RitaKvadrat(Sida, tecken, mellanrum);
+        }
+
+        static void RitaKvadrat(int Sida, string tecken, int mellanrum)
+        {
+            for(int i = 0; i < Sida; i++)
+            {
+                for(int j = 0; j < Sida; j++)
+                {
+                    Console.Write(tecken);
+                    for(int k = 0; k <mellanrum; k++)
+                        Console.Write(" ");
+
+                }
+
+                for (int k = 0; k <= mellanrum; k++)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+
+    }
+}
